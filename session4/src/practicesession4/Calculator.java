628b4a21e0a85fdab2c79c116400dd81796bc7fe
Priyanshi <Priyanshi@DESKTOP-0VP4VA7.fios-router.home>
2017-11-07 10:57:09 -0500

    New Setup First Commit

---
+++ session4/src/practicesession4/Calculator.java
@@ -0,0 +1,33 @@
+package practicesession4;
+
+public class Calculator {
+
+	public int add(int i, int j) {
+		return i + j;
+	}
+
+	public int substract(int i, int j) {
+		return i - j;
+	}
+
+	public int multiply(int i, int j) {
+		return i * j;
+	}
+
+	public double divide(int i, int j) {
+
+		if (j > 0) {
+			return i * 1.0 / j;
+		} else {
+			return 0;
+		}
+	}
+
+	public int mod(int i, int j) {
+		if (j > 0) {
+			return i % j;
+		} else {
+			return 0;
+		}
+	}
+}
---
+++ session4/src/practicesession4/CalculatorRunner.java
@@ -0,0 +1,23 @@
+package practicesession4;
+
+public class CalculatorRunner {
+
+	public static void main(String[] args) {
+		int i = 200;
+		int j = 230;
+
+		Calculator calc = new Calculator();
+		int result = calc.add(i, j);
+		int result1 = calc.substract(i, j);
+		int result2 = calc.multiply(i, j);
+		double result3 = calc.divide(i, j);
+		int result4 = calc.mod(i, j);
+		System.out.println("Addition of two integer variables "+i+" and "+ j+" = " + result);
+		System.out.println("Substraction of two integer variables "+i+" and "+ j+" = " + result1);
+		System.out.println("Multiplication of two integer variables "+i+" and "+ j+" = "+ result2);
+		System.out.println("Division of two integer variables "+i+" and "+ j+" = " + result3);
+		System.out.println("MOD which is remainder of division between two integer variables "+i+" and "+ j+" = "+result4);
+
+	}
+
+}
