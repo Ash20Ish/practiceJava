c19ec0e66da9eb3a829f65afda4877968e073db3
Priyanshi <Priyanshi@DESKTOP-0VP4VA7.fios-router.home>
2017-11-08 00:42:00 -0500

    Updates to session5

--- session4/src/session5/CamparatorRunner.java
+++ session4/src/session5/CamparatorRunner.java
@@ -1,14 +1,13 @@
 package session5;
 
 public class CamparatorRunner {
-	static int h;
 
 	public static void main(String[] args) {
 
 		CompareValue cv = new CompareValue();
 		cv.great(10, 15, 20, 1, 3);
 		cv.low(10, 15, 20, 1, 3);
-		int[] nmbrs = { 1, 4, 2, 8, 3, 6, 10 };
+		int[] nmbrs = { 1, -4, 20, 8, 30, 6, 10 };
 		cv.maxValue(nmbrs);
 		cv.minValue(nmbrs);
 
