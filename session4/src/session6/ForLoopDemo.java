8c87e69ee75fb6f460d57fbcbb45d261ab6e9d1e
Priyanshi <Priyanshi@DESKTOP-0VP4VA7.fios-router.home>
2017-11-09 21:34:30 -0500

    Updated ForLoopDemo

--- session4/src/session6/ForLoopDemo.java
+++ session4/src/session6/ForLoopDemo.java
@@ -4,7 +4,7 @@ public class ForLoopDemo {
 
 	public void nmbrsDivByFiveAndThree(int range) {
 		for (int i = 1; i <= range; i++) {
-			if (i % 3 == 0 || i % 5 == 0) {
+			if ((i % 3 == 0) || (i % 5 == 0)) {
 				System.out.println(i);
 			}
 		}
